bddf2422e3ed5690cbaa39d5b6e5de7ae00f4afb
AbdelrahmanAhmed26 <abdo2642001@gmail.com>
2023-09-25 12:11:02 +0300

    task 9

---
+++ app.js
@@ -0,0 +1,58 @@
+let Color = require("./models/color")
+var mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://meanStack:abdelrahman2642001@cluster0.y8fpmfz.mongodb.net/product"
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("error connection to database");
+  });
+
+const express = require("express");
+// import express from "express";
+
+const server = express();
+server.get("/colors", function (req, res) {
+  Color.find()
+    .then((productsData) => {
+      res.send(productsData);
+    })
+    .catch((err) => {
+      res.send({
+        error: "Error getting product",
+      });
+    });
+});
+server.get("/color/:id", function (req, res) {
+  let colorId = +req.params.id;
+  Color.findOne({ id: colorId },{_id:0})
+    .then((singleProduct) => {
+      res.send(singleProduct);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+server.get("/home", function (req, res) {
+  res.send("<b>Welcome to our APIs</b>")
+});
+
+
+server.get("/", function (req, res) {
+  res.redirect("/home");
+});
+
+
+server.get("*", function (req, res) {
+  res.status(404).send("404 Not Found");
+});
+
+
+
+server.listen(4000, function () {
+  console.log("server connected");
+});
